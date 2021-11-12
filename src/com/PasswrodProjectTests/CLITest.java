4b807b906e3a6be2ab0241abd45e86a51bce175f
Parodix007 <sebastian.siarczynski@gmail.com>
2021-11-12 16:38:36 +0100

    Wyjsciowa struktura projektu, watpie zeby sie zmieniala

---
+++ src/com/PasswrodProjectTests/CLITest.java
@@ -0,0 +1,15 @@
+package com.PasswrodProjectTests;
+
+import com.PasswordProject.CLIClass;
+import org.junit.jupiter.api.Test;
+
+public class CLITest {
+
+    /**
+     *  <p></p>
+     */
+    @Test
+    private void validParameters() {
+
+    }
+}
